09beba7af497647a31c9851070efad50d316148d
maskitegor <maskit.egor@gmail.com>
2023-10-12 22:58:26 +0300

    display error message

--- public/bridge.js
+++ public/bridge.js
@@ -1,7 +1,8 @@
-// Handle events from client
-function trigger(eventName, args) { // eslint-disable-line
-    try {var handlers = window.EventManager.events[eventName];
-        handlers.forEach(handler => handler(JSON.parse(args)));
-    } catch (e) {
-    }
+function trigger(eventName, args) {
+	try {
+		var handlers = window.EventManager.events[eventName];
+		handlers.forEach(handler => handler(JSON.parse(args)));
+	} catch (e) {
+		console.log(e, 'error message')
+	}
 }
